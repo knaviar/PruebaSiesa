ba0a7051ceb8b3eefa4c8027ac79d769535f1b81
knaviar <kathenavia_012@outlook.com>
2024-04-05 16:24:09 -0500

    Algoritmo de division

---
+++ DivisiondeEnteros/DivisiondeEnteros/CambioBases.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisiondeEnteros
+{
+    class CambioBases
+    {
+        public int CambioDeBase(int numero, int b) {
+
+            if
+        }
+
+
+        public int dividir(int a, int b)
+        {
+
+            return a / b;
+
+        }
+
+        public int residuo(int a, int b)
+        {
+
+            return a % b;
+
+        }
+    }
+
+}
---
+++ DivisiondeEnteros/DivisiondeEnteros/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisiondeEnteros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Program programaDivision;
+            int numero1;
+            int numero2;
+            int division;
+
+            programaDivision = new Program(); // instanciar el objeto 
+
+            Console.WriteLine("Ingrese numerador: ");
+            numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese denominador: ");
+            numero2 = int.Parse(Console.ReadLine());
+
+            division = programaDivision.CacularDivision(numero1, numero2);
+            Console.WriteLine("El resultado de la division es: " + division);
+
+            Console.ReadLine(); /*espera a que el usuario presione enter antes de salir*/
+        }
+        public int CacularDivision(int a, int b)
+        {
+
+            return a / b;
+            
+        }
+    }
+}
